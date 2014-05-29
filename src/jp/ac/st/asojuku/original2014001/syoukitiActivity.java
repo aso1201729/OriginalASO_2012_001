c63f42a85576f72713847a41bb304bede577d422
student <student@PC111S13.fgakusei.local>
2014-05-29 15:17:48 +0900

    おみくじ！

---
+++ src/jp/ac/st/asojuku/original2014001/syoukitiActivity.java
@@ -0,0 +1,12 @@
+package jp.ac.st.asojuku.original2014001;
+
+import android.os.Bundle;
+
+public class syoukitiActivity extends MainActivity{
+
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.syoukiti);
+	}
+
+}
